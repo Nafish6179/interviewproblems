7a0b0da6029d334114ddc70db39a1145fd94cb50
Nafish6179 <snafish14@gmail.com>
2026-09-20 19:02:47 +0530

    Added new branch

---
+++ Prg01/src/prog04.java
@@ -0,0 +1,9 @@
+public class prog04 {
+    static String a = "hie i am nafish";
+    static String b = "finexta";
+    static String c = "shai sree hostal";
+
+    public static void main(String[] args) {
+
+    }
+}
