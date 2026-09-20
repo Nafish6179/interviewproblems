1aba936c836937ab194db8c41196559381177992
Nafish6179 <snafish14@gmail.com>
2026-09-20 19:59:01 +0530

    i change the those things, how to resolve

--- Prg01/src/pr03.java
+++ Prg01/src/pr03.java
@@ -1,10 +1,10 @@
 public class pr03 {
     String name = "i am nafish S";
-   static int a = 101;
-    static double b = 2031.0;
+   static int a = 184672;
+    static double b = 5688373.0;
 
     public static void main(String[] args) {
-        System.out.println("i am nafish");
+        System.out.println("i am arish");
         System.out.println("a :"+ a);
         System.out.println("b :"+b);
     }
