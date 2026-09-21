6be09cdb8f15cbc8f0bcb61d254158dfa8b8a382
Nafish6179 <snafish14@gmail.com>
2026-09-21 07:08:11 +0530

    features2 two lines are added to check

--- Prg01/src/pr03.java
+++ Prg01/src/pr03.java
@@ -8,5 +8,7 @@ public class pr03 {
 
         System.out.println("this features 2 branch i want to change something");
         System.out.println("this is brach to merge the main folder");
+        System.out.println("this features two branch two check first");
+        System.out.println(" to check and merge the folder, features02 to main brach");
     }
 }
